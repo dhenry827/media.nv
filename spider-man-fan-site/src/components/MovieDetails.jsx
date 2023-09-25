49b7b5686726e5edd840ac09c1010aa20adca608
COR Coding <corcoding@coding-14.attlocal.net>
2023-09-25 02:16:17 -0400

    Sets up Movies component that renders spiderman movie posters and titles from an api. Then adds the ability to click on movie titles to generate details about selected movie.

--- spider-man-fan-site/src/components/MovieDetails.jsx
+++ spider-man-fan-site/src/components/MovieDetails.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+
 
 const MovieDetails = () => {
+  const [movieInfo, setMovieInfo] = useState({})
+
+  const getMovieInfo = async(id) => {
+    let response = await fetch(`http://www.omdbapi.com/?i=${id}&apikey=4ad02d8d`)
+    let data = await response.json()
+    console.log(data)
+    setMovieInfo(data)
+  }
+
+  // useEffect(() => {
+  //   getMovieInfo()
+  // },[])
+
   return (
-    <div>MovieDetails</div>
+    <div>
+      <div>
+        <img src={movieInfo.Poster} />
+      </div>
+      <div>
+        <h2>{movieInfo.Title} ({movieInfo.Year}) {movieInfo.Rated}</h2>
+        <h3>Director: {movieInfo.Director}</h3>
+        <h3>Genre: {movieInfo.Genre}</h3>
+        <h3>Runtime: {movieInfo.Runtime}</h3>
+        <h3>Rating: {movieInfo.imdbRating}</h3>
+        <h4>Synopsis:</h4><p>{movieInfo.Plot}</p>
+      </div>
+      
+    </div>
   )
 }
 
--- spider-man-fan-site/src/components/Movies.jsx
+++ spider-man-fan-site/src/components/Movies.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+
+import './Movies.css'
 
 const Movies = () => {
+  const [movies, setMovies] = useState([])
+  const [movieInfo, setMovieInfo] = useState({})
+  const [hidden, setHidden] = useState(true)
+
+  const getMovies = async() => {
+    let response = await fetch('http://www.omdbapi.com/?s=spider-man&apikey=4ad02d8d')
+    let data = await response.json()
+    console.log(data)
+    setMovies(data.Search)
+  }
+
+  const getMovieInfo = async(id) => {
+    let response = await fetch(`http://www.omdbapi.com/?i=${id}&apikey=4ad02d8d`)
+    let data = await response.json()
+    setMovieInfo(data)
+    setHidden(false)
+  }
+
+  useEffect(() => {
+    getMovies()
+  },[]) 
+  
   return (
-    <div>Movies</div>
+    <div id='mainCont'>
+      <div className='info' style={{display: hidden ? 'none' : 'flex' }}>
+        <div>
+          <img className='infoPoster' src={movieInfo.Poster} />
+        </div>
+        <div>
+          <h1 className='infoTitle'>{movieInfo.Title} ({movieInfo.Year}) {movieInfo.Rated}</h1>
+          <h3>Director: {movieInfo.Director}</h3>
+          <h3>Genre: {movieInfo.Genre}</h3>
+          <h3>Runtime: {movieInfo.Runtime}</h3>
+          <h3>Rating: {movieInfo.imdbRating}</h3>
+          <h4>Synopsis:</h4><p>{movieInfo.Plot}</p>
+        </div>
+      </div>
+      <div id='movieCont'>
+      {movies.map((movie, index) => {
+        return (
+          <div key={index} className='movie'>
+              <img src={movie.Poster} alt="" className='moviePoster'/>
+              <h3><a className={index} href='#' onClick={(e) => {
+            e.preventDefault(); getMovieInfo(movie.imdbID)}}>{movie.Title} </a>({movie.Year})</h3>
+          </div>
+        ) 
+      })}
+      </div>
+    </div>
   )
 }
 
